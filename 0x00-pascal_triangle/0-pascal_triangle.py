a11d58a00160cf58fe11d0f2843deff9fbd83cff
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-01-04 18:20:00 +0200

    fixed all pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,24 +2,30 @@
 """
 Pascal triangle
 """
+
+
 def pascal_triangle(n):
-  """
-  Uses Combination to get the pascal triangle
-  """
-  if n <= 0:
-    return []
-  lst = []
-  for i in range(n):
-    row = []
-    for j in range(i + 1):
-      row.append((my_factorial(i))//(my_factorial(j) * my_factorial(i - j)))
-    lst.append(row)
-  return lst
+    """
+    Uses Combination to get the pascal triangle
+    """
+    if n <= 0:
+        return []
+    lst = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(
+                (my_factorial(i)) //
+                (my_factorial(j) * my_factorial(i - j))
+                )
+        lst.append(row)
+    return lst
+
 
 def my_factorial(n):
-  """
-  Custom factorial function
-  """
-  if n <= 0:
-    return 1
-  return(n * my_factorial(n-1))
+    """
+    Custom factorial function
+    """
+    if n <= 0:
+        return 1
+    return (n * my_factorial(n-1))
