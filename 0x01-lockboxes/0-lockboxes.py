8f4d81a2433200f0cacccb4c322a123c153025eb
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-01-09 21:39:53 +0200

    All boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Checking if all boxes can be opened by keys inside each
+"""
+def canUnlockAll(boxes):
+    """
+    Returns True if all boxes given can be opened with given keys in each box
+    """
+    obtained_keys = set(boxes[0])
+    filter(obtained_keys, boxes) ## filter unnecessary keys
+    print(f"length of boxes is {len(boxes)}")
+
+    while (len(obtained_keys) + 1 < len(boxes)):
+        temp = set(obtained_keys)
+        for key in temp:
+            obtained_keys.update(boxes[key])
+        filter(obtained_keys, boxes)
+        print(f"temp is '{temp}'")
+        print(f"new set of keys is '{obtained_keys}'")
+        if len(temp) == len(obtained_keys):
+            """
+            If after opening all boxes with given keys,
+            no more obtained keys, then I'm stuck in a loop,
+            and hence, I can't unlock them all
+            """
+            print("no updates")
+            return False
+
+    return True
+
+def filter(set_list, goal_list):
+    """
+    Filter keys so only relevant ones remain
+    """
+    temp = set(set_list)
+    for key in temp:
+        if (key >= len(goal_list) or key == 0):
+            set_list.remove(key)
