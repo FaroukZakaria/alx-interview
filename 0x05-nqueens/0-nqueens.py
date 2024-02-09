0080df95b45091650dab316666a4309d6718faf7
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-02-09 13:01:31 +0200

    Beginning code

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+N queens puzzle. Place N numbers of queens in 
+"""
+
+import sys
+
+
+if len(sys.argv) != 2:
+    print("usage: nqueens N")
+    sys.exit(1)
+
+try:
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
