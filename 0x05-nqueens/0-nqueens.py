6181c8f8f5eac0bcf6e511ce92d3134335789696
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-02-12 19:59:05 +0200

    Capital u for "usage"

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -8,7 +8,7 @@ import sys
 
 
 if len(sys.argv) != 2:
-    print("usage: nqueens N")
+    print("Usage: nqueens N")
     sys.exit(1)
 
 try:
@@ -18,3 +18,46 @@ try:
 except ValueError:
     print("N must be a number")
     sys.exit(1)
+
+def clear(x,y,N):
+    """
+    Clears the boards that are occupied by queen
+    on a position given by (x,y) in NxN board
+    """
+    if ((x > N - 1) or (y > N - 1) or (x < 0) or (y < 0)):
+        return
+
+    positions = set()
+    for i in range(N):
+        positions.add((i,y))  # Horizontal occupied
+        positions.add((x,i))  # Vertical occupied
+
+    X = x
+    Y = y
+    while ((X >= 0) and (Y <= 3)):  # Up-left diagonal
+        positions.add((X, Y))
+        X -= 1
+        Y += 1
+    
+    X = x
+    Y = y
+    while ((X >= 0) and (Y >= 0)):  # Bottom-left diagonal
+        positions.add((X, Y))
+        X -= 1
+        Y -= 1
+
+    X = x
+    Y = y
+    while ((X <= 3) and (Y <= 3)):  # Top-right diagonal
+        positions.add((X, Y))
+        X += 1
+        Y += 1
+    
+    X = x
+    Y = y
+    while ((X <= 3) and (Y >= 0)):  # Bottom-right diagonal
+        positions.add((X, Y))
+        X += 1
+        Y -= 1
+
+    return [list(t) for t in positions]
