d6a9387501a21be954e0f155e07905cbad8a7f24
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-02-13 00:26:49 +0200

    oh right... removing the debugging prints

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -80,26 +80,17 @@ i = 0
 j = 0
 while i < N:
     while j < N:
-        print("current is now:")
-        print(current)
         if ([i, j] in occupied):
-            print("occupied")
             if j == N - 1:  # Check if y-position is at top
-                print("It's on top as well")
                 i -= 1
                 occupied = occupied[:-(len(clear(i, current[-1][-1], N)))]
                 j = current[-1][-1]
                 current.pop()
-                print("after going back and undoing, current is now:")
-                print(current)
                 if j == N - 1:  # Check if y-position at top after change
-                    print("the one at back is already at top...")
                     if i == 0:  # Check if x-position at start (all done)
-                        print("and it's at first field as well.. game over")
                         the_end = 1
                         break
                     else:  # There are more possibilities
-                        print("and there are more possibilities")
                         i -= 1
                         occupied = occupied[:-(len(clear(
                             i,
@@ -107,44 +98,29 @@ while i < N:
                         j = current[-1][-1]
                         current.pop()
                         j += 1
-                        print("after going back and undoing, current is now:")
-                        print(current)
                         continue
                 else:  # if y not at top
-                    print("moving up")
                     j += 1
                     continue
             else:  # if y not at top
-                print("moving up")
                 j += 1
                 continue
         else:  # Not occupied
-            print(f"Not occupied. Marking... {[i,j]}")
             current.append([i, j])  # mark the point
             if i == N - 1:  # Checks if all queens are placed
-                print("all queens are placed. Submitting solution:")
-                print(current)
                 solutions.append(current)  # Add the solution
                 j = current[0][-1] + 1  # start a new game
                 i = 0
                 current = []
                 occupied = []
-                print("starting a new game")
                 continue
             for elm in clear(i, j, N):  # mark all the occupied squares
                 occupied.append(elm)
             i += 1
             j = 0
-            print("Done marking occupied squares...")
-            print(occupied[:-(len(clear(i, current[-1][-1], N)))])
-            print("total occupied:")
-            print(occupied)
-            print("moving to next field")
             break
-    # print("blax")
     if the_end == 1:
         break
-    print("another round starts")
 
 for i in solutions:
     print(i)
