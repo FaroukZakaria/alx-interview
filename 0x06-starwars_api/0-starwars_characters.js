4b99291bd31887605d4dd9f5cce7cd91f38c220f
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-02-19 00:53:58 +0200

    Star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(movieUrl, { json: true }, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const characters = body.characters;
+
+    characters.forEach(characterUrl => {
+        request(characterUrl, { json: true }, (error, response, characterBody) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            console.log(characterBody.name);
+        });
+    });
+});
