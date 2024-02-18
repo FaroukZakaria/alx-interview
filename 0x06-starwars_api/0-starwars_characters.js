58638d03cf1994c844d5c1e3f24a7d1858ab3efa
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-02-18 15:05:14 -0800

    semistandards

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,20 +7,21 @@ const movieId = process.argv[2];
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(movieUrl, { json: true }, (error, response, body) => {
-    if (error) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const characters = body.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, { json: true }, (error, response, characterBody) => {
+      if (error) {
         console.error(error);
         return;
-    }
-
-    const characters = body.characters;
-
-    characters.forEach(characterUrl => {
-        request(characterUrl, { json: true }, (error, response, characterBody) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            console.log(characterBody.name);
-        });
+      }
+      console.log(characterBody.name);
     });
+  });
 });
+
