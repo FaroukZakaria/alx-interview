dd75c622906ea02c05643baeddb144ec8a9a1d92
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-02-26 12:49:43 +0000

    Fixed mistakes I did (debugger still exists)

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,20 +17,20 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    impossible = 1
+    """impossible = 1
     for i in coins:
         if total % i == 0:
             impossible = 0
             break
 
     if impossible:
-        return -1
+        return -1"""
 
     sorted_coins = sorted(coins)
     total_coins = total
     print(f"Calculating for coins: {sorted_coins}")
     number_of_coins = 0
-    for i in range(-1, len(sorted_coins) * -1, -1):
+    for i in range(-1, len(sorted_coins) * -1 - 1, -1):
         print(f"calculating {total_coins} / {sorted_coins[i]}")
         number_of_coins += (total_coins // sorted_coins[i])
         print(f"subtracting "
@@ -39,5 +39,8 @@ def makeChange(coins, total):
         total_coins -= (total_coins // sorted_coins[i]) * sorted_coins[i]
         print(f"and total coins now are {total_coins}")
         print(f"number of coins is now: {number_of_coins}")
+        
+        if total_coins == 0:
+            return number_of_coins
 
-    return number_of_coins
+    return -1
