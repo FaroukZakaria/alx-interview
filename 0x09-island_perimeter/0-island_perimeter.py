3e88e56efa9be0d49238cae4df0a15da47e946d8
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-05 14:45:25 +0000

    Testing and fixing. Found error (debuggers exist)

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
 This module calculates perimeter of an island represented by 1's
-in water represented by 0's. There's only one island (or nothing).
+in solid represented by 0's. There's only one island (or nothing).
 
 Each "1" is a square with edge length of 1 unit.
 """
-
 
 def island_perimeter(grid):
     """
@@ -21,31 +20,49 @@ def island_perimeter(grid):
 
     perimeter = 0
     for row in range(height):
+        print(f"checking for row: {row}")
         for elm in range(width):
             if grid[row][elm] == 0:
                 continue
 
-            edge = 0
+            solid = 0
             # Check up
             if row != 0:
                 if grid[row - 1][elm] == 0:  # There's water above
-                    edge += 1
+                    print("there's water above")
+                    solid += 1
+            else:
+                print("there's water above (beyond borders)")
+                solid += 1
 
             # Check left
             if elm != 0:
                 if grid[row][elm - 1] == 0:  # There's water left
-                    edge += 1
+                    print("there's water left")
+                    solid += 1
+            else:
+                print("there's water left (beyond borders)")
+                solid += 1
 
             # Check right
             if elm != width - 1:
                 if grid[row][elm + 1] == 0:  # There's water right
-                    edge += 1
+                    print("there's water right")
+                    solid += 1
+            else:
+                print("there's water right (beyond borders)")
+                solid += 1
 
             # Check down
             if row != height - 1:
                 if grid[row + 1][elm] == 0:  # There's water below
-                    edge += 1
+                    print("there's water below")
+                    solid += 1
+            else:
+                print("there's water below (beyond borders)")
+                solid += 1
 
-            perimeter += edge
+            print(f"adding {solid} to perimeter")
+            perimeter += solid
 
     return perimeter
