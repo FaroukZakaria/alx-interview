c1eb8d46f502e5e788a3105c677b9dc5d4459557
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-13 20:22:33 +0000

    With test cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -49,6 +49,18 @@ def isWinner(x, nums):
     """
     Returns the winner of the game (or None if tie)
     """
+    if x > len(nums):  # number of rounds is more than given n for each round
+        return None
+
+    if nums == []:  # Empty list means no given n to play the game
+        return None
+
+    if not isinstance(nums, list):  # if nums isn't a list
+        return None
+
+    for i in range(len(nums)):  # Check if any list item is not an integer
+        if not isinstance(nums[i], int):
+            return None
     Ben = 0
     Maria = 0
     for i in range(x):
---
+++ 0x0A-primegame/test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Test cases for prime game
+"""
+
+
+import unittest
+
+isWinner = __import__('0-prime_game').isWinner
+
+class TestWinner(unittest.TestCase):
+
+    def test_odd_primes(self):
+        self.assertEqual(isWinner(5, [1, 1, 1, 1, 1]), "Ben")
+
+    def test_even_primes(self):
+        self.assertEqual(isWinner(5, [2, 2, 2, 2, 2]), "Maria")
+
+    def test_not_equal_rounds(self):
+        self.assertEqual(isWinner(3, [1, 1, 1, 2, 2, 2, 2]), "Ben")
+
+    def test_not_equal_rounds_2(self):
+        self.assertEqual(isWinner(5, [1, 1, 1]), None)
+
+    def test_tie(self):
+        self.assertEqual(isWinner(0, [1, 1, 1]), None)
+
+    def test_odd_very_big(self):
+        self.assertEqual(isWinner(1, [9967]), "Ben")
+
+    def test_even_very_big(self):
+        self.assertEqual(isWinner(1, [10000]), "Maria")
+
+    def test_no_primes_1(self):
+        self.assertEqual(isWinner(1, [-1]), "Ben")
+
+    def test_no_primes_2(self):
+        self.assertEqual(isWinner(1, [0]), "Ben")
+
+    def test_no_primes_3(self):
+        self.assertEqual(isWinner(1, []), None)
+
+    def test_wrong_argument_1(self):
+        self.assertEqual(isWinner(1, "Hello"), None)
+
+    def test_wrong_argument_2(self):
+        self.assertEqual(isWinner(1, ["Hello"]), None)
+
+    def test_wrong_argument_3(self):
+        self.assertEqual(isWinner(2, [1, "Hello"]), None)
+if __name__ == '__main__':
+    unittest.main()
